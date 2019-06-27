69702e2a16dd1750d92b46abf04b9c72e2e3e34e
QuocViet <quocvietht03@gmail.com>
2019-06-27 09:42:53 +0700

    fix testimonial home 5

--- attachment.php
+++ attachment.php
@@ -10,7 +10,7 @@
 					while ( have_posts() ) : the_post();
 						if ( wp_attachment_is_image( get_the_ID() ) ) {
 							$att_image = wp_get_attachment_image_src( get_the_ID(), "full");
-							echo '<img src="'.esc_attr($att_image[0]).'" width="'.esc_attr($att_image[1]).'" height="'.esc_attr($att_image[2]).'"  class="attachment-medium" alt="'.esc_attr__('Thumbnail', 'ametex').'" />';
+							echo '<img src="'.esc_url($att_image[0]).'" width="'.esc_attr($att_image[1]).'" height="'.esc_attr($att_image[2]).'"  class="attachment-medium" alt="'.esc_attr__('Thumbnail', 'ametex').'" />';
 						}
 					endwhile;
 					?>
--- comments.php
+++ comments.php
@@ -49,9 +49,9 @@ if ( post_password_required() ) {
 		$commenter = wp_get_current_commenter();
 		
 		$fields =  array(
-			'author' => '<div class="row"><div class="col-md-4 comment-form-author"><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" placeholder="'.__('Name*','ametex').'" aria-required="true" /></div>',
-			'email' => '<div class="col-md-4 comment-form-email"><input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" placeholder="'.__('Email*','ametex').'" aria-required="true" /></div>',
-			'url' => '<div class="col-md-4 comment-form-url"><input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" placeholder="'.__('Website','ametex').'" /></div></div>',
+			'author' => '<div class="row"><div class="col-md-4 comment-form-author"><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" placeholder="'.esc_attr__('Name*','ametex').'" aria-required="true" /></div>',
+			'email' => '<div class="col-md-4 comment-form-email"><input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" placeholder="'.esc_attr__('Email*','ametex').'" aria-required="true" /></div>',
+			'url' => '<div class="col-md-4 comment-form-url"><input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" placeholder="'.esc_attr__('Website','ametex').'" /></div></div>',
 		);
 		
 		$args = array(
